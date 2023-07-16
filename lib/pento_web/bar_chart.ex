804a05ae96342b952913426277cc4128c7d3271b
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-07-16 15:17:15 -0300

    Pack Chart Code as a Macro

---
+++ lib/pento_web/bar_chart.ex
@@ -0,0 +1,33 @@
+defmodule PentoWeb.BarChart do
+  alias Contex.Dataset
+  alias Contex.BarChart
+  alias Contex.Plot
+
+  def make_bar_chart_dataset(data) do
+    Dataset.new(data)
+  end
+
+  def make_bar_chart(dataset) do
+    dataset
+    |> BarChart.new()
+  end
+
+  defp chart_helpers do
+    quote do
+      import PentoWeb.BarChart
+    end
+  end
+
+  def chart_live do
+    quote do
+      unquote(chart_helpers())
+    end
+  end
+
+  def render_bar_chart(chart, title, subtitle, x_axis, y_axis) do
+    Plot.new(500, 400, chart)
+    |> Plot.titles(title, subtitle)
+    |> Plot.axis_labels(x_axis, y_axis)
+    |> Plot.to_svg()
+  end
+end
