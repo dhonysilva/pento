b2a61c74cfaf3e2ade3bd8a78c4285bcd1194993
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-07-16 10:22:40 -0300

    Implement Profile context and root layout

--- lib/pento/accounts/profile.ex
+++ lib/pento/accounts/profile.ex
@@ -6,6 +6,7 @@ defmodule Pento.Accounts.Profile do
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
+
   schema "profiles" do
     field :username, :string
     belongs_to :user, User
@@ -16,7 +17,7 @@ defmodule Pento.Accounts.Profile do
   @doc false
   def registration_changeset(profile, attrs) do
     profile
-    |> cast(attrs, [:username])
+    |> cast(attrs, [:username, :user_id])
     |> validate_required([:username])
   end
 end
