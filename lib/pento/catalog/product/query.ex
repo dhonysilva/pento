0faee4bf8dfc2b0b5659da47734dea2f7c5c2fa3
Dhony Silva <dhony.web@gmail.com>
2024-11-06 19:24:30 -0300

    Query for Products Ratings Average

--- lib/pento/catalog/product/query.ex
+++ lib/pento/catalog/product/query.ex
@@ -2,6 +2,8 @@ defmodule Pento.Catalog.Product.Query do
   import Ecto.Query
   alias Pento.Catalog.Product
   alias Pento.Survey.Rating
+  alias Pento.Survey.Demographic
+  alias Pento.Accounts.User
 
   def base, do: Product
 
@@ -15,5 +17,23 @@ defmodule Pento.Catalog.Product.Query do
 
     query
     |> preload(ratings: ^ratings_query)
+  end
+
+  def with_average_ratings(query \\ base()) do
+    query
+    |> join_ratings
+    |> average_ratings
+  end
+
+  def join_ratings(query) do
+    query
+    |> join(:inner, [p], r in Rating, on: r.product_id == p.id)
+  end
+
+  def average_ratings(query) do
+    query
+    |> group_by([p], p.id)
+    |> select([p, r], {p.name, avg(r.starts)})
+    |> order_by([p, r], [{:asc, p.name}])
   end
 end
